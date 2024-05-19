5a484e0c8bfbcb4b983c41f95a4d7e508787bd90
lyingenvelope <147272019+lyingenvelope@users.noreply.github.com>
2024-05-19 14:03:25 -0700

    Update Downlink.py fixed bigendian stuff

--- Downlink.py
+++ Downlink.py
@@ -6,9 +6,9 @@ def output(ser, temps, num_img, stats, imgs_accquired, volts):
     b=bytearray(20)
     b[0:1]=b'\x01' #start symbol
     b[1:9]=bytes(temps) #temperature
-    b[9:11]=num_img.to_bytes(2) #number of images
+    b[9:11]=num_img.to_bytes(2,'big') #number of images
     b[11:14]=bytes(stats) #tracking status, last command status, heartbeat
-    b[14:16]=imgs_accquired.to_bytes(2) #images accquired
+    b[14:16]=imgs_accquired.to_bytes(2,'big') #images accquired
     b[16:18]=bytes(volts) #5volt voltage, 12 volt voltage
     b[18:20]=b'\x0a\x0d' #end symbol
     ser.write(b)
